ceee4739a7c668c1999340ac8b297a57bdb06460
Grigory <grishka5556@gmail.com>
2023-01-07 15:15:56 +0300

    second commit

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import {useSearchUsersQuery} from "../store/github/github.api";
 
 export function HomePage() {
+  const {isLoading, isError, data} = useSearchUsersQuery('gregory')
+
+
   return (
     <div>home</div>
   )
